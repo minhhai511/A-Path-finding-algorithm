a1d804e70449d65e7f5b8cb145e8afbfa15307f9
M1nhair <53269443+minhhai511@users.noreply.github.com>
2022-12-02 11:43:05 +0700

    first commit

---
+++ src/a/star/path/finding/Node.java
@@ -0,0 +1,19 @@
+package a.star.path.finding;
+
+
+public class Node {
+
+    int x;
+    int y;
+    double hValue;
+    int gValue;
+    double fValue;
+    Node parent;
+
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
